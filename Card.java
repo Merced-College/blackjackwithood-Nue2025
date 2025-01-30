bcde48a868b48b52fa03d7a0c9ffd3cf44a9f4e6
Nue <hipilot17@gmail.com>
2025-01-30 22:56:13 +0000

    Added spaces
    Co-authored-by: vvue1234 <vvue1234@users.noreply.github.com>
    Co-authored-by: 3rdkira <3rdkira@users.noreply.github.com>

--- Card.java
+++ Card.java
@@ -24,48 +24,51 @@ public class Card {
         this.picture = picName;
     }
 
-
     //4 setters and 4 getters
+    //4 setters
     public void setSuit(String suit){
         this.suit = suit;
     }
-
     public void setRank(String rank){
         this.rank = rank;
     }
-
     public void setValue(int value){
         this.value = value;
     }
-
     public void setPicture(String picture){
         this.picture = picture;
     }
 
-    //getter:
+    //4 getters
     public String getSuit(){
         return suit;
     }
-
     public String getRank(){
         return rank;
     }
-
     public int getValue(){
         return value;
     }
-
     public String getPicture(){
         return picture;
     }
 
-
     public String toString(){
-        return "suit: " + suit + ", rank: " + rank + ", value: " + value + ", picture: " + picture;
+        if (suit.equals("heart")){
+            suit = suit + "♥";
+        }else if(suit.equals("diamond")){
+            suit = suit + "♦";
+        }else if (suit.equals("club")){
+            suit = suit + "♣";
+        }else if(suit.equals("spade")){
+            suit = suit + "♠";
+        }
+
+        return String.format("suit: %s  rank: %10s  value: %10d  picture: %10s", suit, rank, value, picture);
     }
 
 
-    public boolean equals(Card other){//This sets up the main program where we can compare if any of the cards match together.
+    public boolean equals(Card other){ //This sets up the main program where we can compare if any of the cards match together.
         return rank.equals(other.rank) && value==other.value;
     }
 }
--- CardGame.java
+++ CardGame.java
@@ -1,6 +1,6 @@
-//Nue Lopez, Veejay Vue, Jacob Butler, ...
-//1/28/2025
-//Card Game With OOD
+// Nue Lopez, Veejay Vue, Jacob Butler, ...
+// 1/28/2025
+// Card Game With OOD
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -11,17 +11,18 @@ public class CardGame {
 	//Initializes array...
 	private static ArrayList<Card> deckOfCards = new ArrayList<Card>();
 	private static ArrayList<Card> playerCards = new ArrayList<Card>();
-
+	//                                                                                                                                          private static ArrayList<Card> playerCards2 = new ArrayList<Card>();
 
 	public static void main(String[] args) {
 		Scanner input = null; //Input hasn't been used yet.
-		
+
 		//Added how many cards the player wants.
 		Scanner scnr = new Scanner(System.in);
 		System.out.println("How many cards do you want? ");
 		int numOfCards = scnr.nextInt();
 
-		try {
+		//This checks if there is an error, it will accept it even though there is an error. 
+		try {//Reads in from another text file the cards. It is so that way it doesn't take all the space in the main.
 			input = new Scanner(new File("cards.txt"));
 		} catch (FileNotFoundException e) {
 			// error
@@ -38,12 +39,12 @@ public class CardGame {
 		}
 
 		shuffle(); //Shuffles the cards when called for.
-
 		//for(Card c: deckOfCards)
 			//System.out.println(c);
 
 		//deal the player x amount of cards
 		for(int i = 0; i < numOfCards; i++) {
+			//playerCards.get(i).getSuit(); //Gets suit
 			playerCards.add(deckOfCards.remove(i));
 		}
 		
@@ -51,14 +52,12 @@ public class CardGame {
 		System.out.println("players cards");
 		for(Card c: playerCards)
 			System.out.println(c);
-
+		
 		//Line says if there are any pairs in the decks dealt.
 		System.out.println("pairs is " + checkFor2Kind());
-
 	}//end main
 
 	public static void shuffle() {
-
 		//shuffling the cards by deleting and reinserting
 		for (int i = 0; i < deckOfCards.size(); i++) {
 			int index = (int) (Math.random()*deckOfCards.size());
@@ -70,12 +69,15 @@ public class CardGame {
 
 	//check for 2 of a kind in the players hand
 	public static boolean checkFor2Kind() {
-
 		int count = 0;
 		for(int i = 0; i < playerCards.size() - 1; i++) {
 			Card current = playerCards.get(i);
 			Card next = playerCards.get(i+1);
-			
+
+			//Going to add a three of a kind comparison...?
+			/*
+			Card third = playerCards.get(i+2);
+			*/
 			
 			for(int j = i+1; j < playerCards.size(); j++) {
 				next = playerCards.get(j);
@@ -86,8 +88,8 @@ public class CardGame {
 			}//end of inner for
 			if(count == 1)
 				return true;
-
 		}//end outer for
 		return false;
 	}
+	
 }//end class
